88ea1fefa1b45b7447b63c82634dbf921060150c
Thomas Mailund <mailund@birc.au.dk>
2022-03-14 12:14:03 +0100

    removed solutions

--- src/align.c
+++ src/align.c
@@ -4,37 +4,21 @@
 #include <string.h>
 
 void local_align(const char *p, const char *x, int i, const char *edits,
-                 char **p_row_buf, char **x_row_buf)
+                 char **p_row, char **x_row)
 {
     size_t n = strlen(edits);
-    char *p_row = *p_row_buf = malloc(n + 1);
-    char *x_row = *x_row_buf = malloc(n + 1);
-    assert(p_row && x_row); // Don't want to handle allocation failures
+    *p_row = malloc(n + 1);
+    *x_row = malloc(n + 1);
+    assert(*p_row && *x_row); // Don't want to handle allocation failures
 
-    size_t j = 0, k = 0;
-    for (size_t l = 0; l < n; l++)
-    {
-        switch (edits[l])
-        {
-        case 'M':
-            p_row[l] = p[j++];
-            x_row[l] = x[i + k++];
-            break;
-        case 'I':
-            p_row[l] = '-';
-            x_row[l] = x[i + k++];
-            break;
-        case 'D':
-            p_row[l] = p[j++];
-            x_row[l] = '-';
-            break;
-        }
-    }
-    p_row[n] = x_row[n] = '\0';
+    // Compute the alignment rows
 }
 
 void align(const char *p, const char *q, const char *edits,
            char **p_row, char **q_row)
 {
-    local_align(p, q, 0, edits, p_row, q_row);
+    size_t n = strlen(edits);
+    *p_row = malloc(n + 1);
+    *q_row = malloc(n + 1);
+    // Compute the alignment rows
 }
--- src/edit.c
+++ src/edit.c
@@ -17,61 +17,16 @@ void get_edits(const char *p_row, const char *q_row,
     char *edits = *edits_buf = malloc(n + 1);
     assert(p && q && edits); // Don't want to handle allocation failures
 
-#ifdef EXERCISE
     // FIXME: do the actual calculations here
 
     p[0] = '\0';     // for now we just make
     q[0] = '\0';     // empty strings
     edits[0] = '\0'; // so at least we can run tests.
-#endif
-
-    size_t j = 0, k = 0;
-    for (size_t i = 0; i < n; i++)
-    {
-        if (p_row[i] == '-')
-        {
-            edits[i] = 'I';
-            q[k++] = q_row[i];
-            continue;
-        }
-        if (q_row[i] == '-')
-        {
-            edits[i] = 'D';
-            p[j++] = p_row[i];
-            continue;
-        }
-        edits[i] = 'M';
-        p[j++] = p_row[i];
-        q[k++] = q_row[i];
-    }
-
-    // Terminate strings
-    edits[n] = '\0';
-    p[j] = '\0';
-    q[k] = '\0';
 }
 
 int edit_dist(const char *p, const char *x, int i, const char *edits)
 {
     int dist = 0;
-    int j = 0;
-    for (; *edits; edits++)
-    {
-        switch (*edits)
-        {
-        case 'I':
-            dist++;
-            i++;
-            break;
-        case 'D':
-            dist++;
-            j++;
-            break;
-        case 'M':
-            dist += (x[i] != p[j]);
-            i++;
-            j++;
-        }
-    }
+    // Compute the edit distance
     return dist;
 }
